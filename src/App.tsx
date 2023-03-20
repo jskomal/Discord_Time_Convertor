1b2d14a131548905a48e2b1ef37e2ca2e279c20d
jskomal <90876852+jskomal@users.noreply.github.com>
2023-03-20 13:07:25 -0600

    Add formatting options

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import dayjs from 'dayjs'
 import './App.css'
 
@@ -6,16 +6,21 @@ function App() {
 	const [date, setDate] = useState(dayjs().format('YYYY-MM-DD'))
 	const [time, setTime] = useState('')
 	const [copyText, setCopyText] = useState('')
+	const [formattingText, setFormattingText] = useState('f')
 
 	useEffect(() => {
-		handleParse(date, time)
-	}, [date, time])
+		handleParse(date, time, formattingText)
+	}, [date, time, formattingText])
 
-	const handleParse = (inputDate: string, inputTime: string) => {
+	const handleParse = (
+		inputDate: string,
+		inputTime: string,
+		format: string
+	) => {
 		if (!inputTime) return
 		const [hour, minute] = inputTime.split(':')
 		const now = dayjs(inputDate).hour(parseInt(hour)).minute(parseInt(minute))
-		setCopyText(`<t:${now.unix()}>`)
+		setCopyText(`<t:${now.unix()}:${format}>`)
 	}
 
 	const handleCopy = (text: string) => {
@@ -44,6 +49,15 @@ function App() {
 					value={time}
 					onChange={(e) => setTime(e.target.value)}
 				/>
+				<label htmlFor='formatting'>Formatting Options</label>
+				<select
+					name='formatting'
+					onChange={(e) => setFormattingText(e.target.value)}>
+					<option value='f'>Date + Time</option>
+					<option value='D'>Date only</option>
+					<option value='t'>Time only</option>
+					<option value='R'>Relative Time</option>
+				</select>
 				{copyText ? <h2>{copyText}</h2> : <h2>Please input a time</h2>}
 				<button onClick={() => handleCopy(copyText)} disabled={!copyText}>
 					Copy Timestamp
